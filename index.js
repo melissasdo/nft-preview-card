0be84309a12a25cf6bf6a343f778a4a245ea6a5c
unknown <mmercurydevs>
2023-08-19 15:08:10 -0300

    Initial setup

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
